f99463f078d3257088ee00cf35af570f77cd6b6f
atacan <atacan.sss1@gmail.com>
2024-09-06 02:36:09 +0300

    Merge branch 'main' of https://github.com/gurbuzz/AI-image-processing

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,3 @@ Route::post('images', [ImageController::class, 'store'])->name('images.store');
 Route::delete('images/{id}', [ImageController::class, 'destroy'])->name('images.destroy');
 route::get('/analyze-image/{filename}', [ImageController::class, 'analyzeImage'])->name('images.analyze');
 route::get('images/{id}/analyze', [ImageController::class, 'analyze'])->name('images.analyze');
-
